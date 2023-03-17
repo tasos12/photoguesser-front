747be2a207fd30cc4800aafe9b46895af315abf0
Tasos Gkagkas <tasosggps@gmail.com>
2023-03-17 23:23:24 +0200

    Add vercel analytics to application

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import "@fontsource/public-sans";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
