b2291be974032d7923d3e4f60f97290aae3725ee
Tasos Gkagkas <tasosggps@gmail.com>
2023-03-29 22:29:59 +0300

    Add footer sematic tag

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -2,7 +2,7 @@ import Typography from "@mui/joy/Typography";
 
 export default function Footer() {
     return(
-        <Typography sx={{ fontSize: "10px", color: "black", textAlign: "center" }}>
+        <Typography component={"footer"} sx={{ fontSize: "10px", color: "black", textAlign: "center" }}>
             © Created by Tasos Gkagkas
         </Typography>
     )
